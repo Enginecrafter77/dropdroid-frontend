c4996d0c2303cdf5397053db655aa8e1a2b021df
Enginecrafter77 <hutiramichal@gmail.com>
2024-10-22 22:41:52 +0200

    fix: Add vite-vuetify-plugin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import plugins from "./plugins";
 
 const app = createApp(App);
 app.use(router);
+app.use(plugins.vuetify);
 app.mount("#app");
