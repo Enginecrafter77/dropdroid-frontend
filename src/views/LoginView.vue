fd0212c9c71916a38a5f614b56a9950620751225
m-dano <martin.dano3@gmail.com>
2024-11-11 11:03:16 +0100

    feat: update login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,7 +4,7 @@
     const tab = ref();
 </script>
 <template>
-    <div class="login d-flex flex-column justify-center align-center">
+    <div class="login d-flex flex-column justify-center align-center ga-3">
         <div class="d-flex flex-row flex-1-0 justify-center align-center ga-10 pt-16 pb-8">
             <v-img aspect-ratio="1/1" :inline="true" class="page-icon" rounded="circle" src="/public/logo.png"/>
             <span class="page-title">DropDroid</span>
@@ -14,6 +14,9 @@
         <v-text-field class="w-33" color="primary" type="password" label="Password"></v-text-field>
         <v-btn to="/" class="w-25"rounded="lg" elevation="8" color="primary">
             Log in
+        </v-btn>
+        <v-btn to="/register" variant="outlined" class="w-10 p-5"rounded="lg" color="primary">
+            Register
         </v-btn>
     </div>
 </template>
