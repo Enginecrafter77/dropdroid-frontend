bdbf6bab984d3fbbbbe77a12cf68bf4961610a65
Enginecrafter77 <hutiramichal@gmail.com>
2024-12-03 14:05:45 +0100

    refactor: Converted types to classes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,46 +1,69 @@
-export type User = {
-    id: number,
-    username: string,
-    display_name: string,
-    role: string,
-    avatar_url: string
+export enum ServerRole {
+    USER = "user",
+    ADMIN = "admin"
 };
 
-export type Organization = {
-    id: number,
-    slug: string,
-    name: string,
-    description: string|undefined,
-    icon_url: string
+export enum OrganizationRole {
+    MEMBER = "member",
+    ADMIN = "admin"
 };
 
-export type OrganizationMembership = {
-    id: number,
-    user: User,
-    organization: Organization,
-    role: string
+export class User {
+    id: number = 0;
+    username: string = "new_user";
+    display_name: string = "New User";
+    role: ServerRole = ServerRole.USER;
+    avatar_url: string = "https://null.null/null.png";
 };
 
-export type PaginationResponse<T> = {
-    total: number,
-    per_page: number,
-    current_page: number,
-    last_page: number,
-    first_page_url: string,
-    last_page_url: string,
-    next_page_url: string|null,
-    prev_page_url: string|null,
-    path: string,
-    from: number,
-    to: number,
-    data: T[]
+export class Organization {
+    id: number = 0;
+    slug: string = "new_organization";
+    name: string = "New Organization";
+    description: string|undefined = "An example application";
+    icon_url: string = "https://null.null/null.png";
 };
 
-export type Application = {
-    id: number,
-    name: string,
-    namespace: string,
-    description: string|null,
-    icon_url: string,
-    organization: Organization
+export class Application {
+    id: number = 0;
+    name: string = "New Application";
+    namespace: string = "com.example.new_app";
+    description: string|null = "Here goes description";
+    icon_url: string = "https://null.null/null.png";
+    organization: Organization|undefined = undefined;
+};
+
+export class Package {
+    id: number = 0;
+    label: string|null = null;
+    version_name: string = "1.0.0";
+    version_code: number = 100;
+    downloads: number = 0;
+    created_at: number = 0;
+    flavor: string = "main";
+    variant: string = "release";
+    file_url: string = "https://null.null/null.apk";
+    application: Application|undefined = undefined;
+};
+
+export class OrganizationMembership {
+    id: number = 0;
+    role: OrganizationRole = OrganizationRole.MEMBER;
+    user: User|undefined = undefined;
+    organization: Organization|undefined = undefined;
+};
+
+export class PaginationResponse<T> {
+    total: number = 0;
+    per_page: number = 0;
+    current_page: number = 0;
+    last_page: number = 0;
+    first_page_url: string = "";
+    last_page_url: string = "";
+    next_page_url: string|null = null;
+    prev_page_url: string|null = null;
+    path: string = "";
+    from: number = 0;
+    to: number = 0;
+    data: T[] = []
 };
