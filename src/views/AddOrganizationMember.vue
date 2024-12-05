9d49ca74d87c4cb14d1ba5d95b8d875e1a514399
m-dano <martin.dano3@gmail.com>
2024-12-05 17:59:17 +0100

    feat: OrganizationMember and PasswordChangeDialog component + AddOrganizationMember, Organization, Profile views

--- src/views/AddOrganizationMember.vue
+++ src/views/AddOrganizationMember.vue
@@ -2,7 +2,9 @@
 import ItemUser from '@/components/ItemUser.vue';
 import type { OrganizationMembership, PaginationResponse, User } from '@/types';
 import { mapByProperty } from '@/utils';
+import { computed } from '@vue/reactivity';
 import type { AxiosInstance } from 'axios';
+import { isArrowFunction } from 'typescript';
 import { inject, ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
@@ -17,6 +19,13 @@ import { useRoute, useRouter } from 'vue-router';
     });
 
     const users = ref<User[]>([]);
+
+    const filteredUsers = computed<User[]>(() => {
+        if(search.value === "")
+            return users.value;
+        return Array.from(users.value.filter((f) => f.username.includes(search.value) || f.display_name.includes(search.value)));
+    });
+    const search = ref<string>("");
     async function loadData(){
         if (apiClient === undefined)
         {
@@ -50,8 +59,21 @@ import { useRoute, useRouter } from 'vue-router';
                     
                 </div>
             </v-col>
+            <v-col cols="12">
+                <div class="d-flex flex-column justify-start align-center ga-4">
+                    <v-text-field
+                        color="primary"
+                        label="Search"
+                        width="100%"
+                        max-width="40rem"
+                        variant="outlined"
+                        prepend-inner-icon="mdi-magnify"
+                        v-model="search"
+                    />
+                </div>
+            </v-col>
             <v-col
-                v-for="user of users"
+                v-for="user of filteredUsers"
                 md="6"
                 :key="user.id"
                 cols="12"
