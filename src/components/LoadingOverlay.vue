2726f2084ec2fa00d34f0ef6f69e175ee78c62cd
Enginecrafter77 <hutiramichal@gmail.com>
2024-12-05 01:14:27 +0100

    feat: LoadingOverlay

---
+++ src/components/LoadingOverlay.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+    defineProps({
+        loading: {
+            type: Boolean,
+            default: false
+        }
+    });
+</script>
+
+<template>
+    <div class="loading-overlay-container">
+        <slot></slot>
+        <div class="loading-overlay" v-if="loading">
+            <div class="w-100 fill-height d-flex flex-col justify-center align-center">
+                <v-progress-circular indeterminate/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .loading-overlay-container {
+        position: relative;
+    }
+    
+    .loading-overlay {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        background-color: rgba(255, 255, 255, 0.8);
+    }
+</style>
